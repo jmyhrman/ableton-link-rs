590d4ba4923f60a64d7a63ccfcda031bf0b8c455
Jimmy Myhrman <jimmy@myhrman.org>
2021-09-07 16:29:37 +0200

    Link-hut example tweak

--- src/bin/link-hut.rs
+++ src/bin/link-hut.rs
@@ -5,15 +5,23 @@ use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
+    let quantum = 4.0;
     let mut link = Link::new(120.0);
     let clock = link.clock();
+    link.enable(true);
     loop {
         link.with_app_session_state(|mut session_state| {
-            let now = clock.micros();
-            println!("now: {:?}", now);
+            let time = clock.micros();
+            let tempo = session_state.tempo();
+            let playing = session_state.is_playing();
+            let beat = session_state.beat_at_time(time, quantum);
+            println!(
+                "playing={}, quantum={}, clock={}, tempo={}, beat={}",
+                playing, quantum, time, tempo, beat
+            );
             sleep(Duration::from_millis(100));
-            session_state.set_tempo(122.0, 0);
-            session_state.commit();
+            //session_state.set_tempo(122.0, 0);
+            //session_state.commit();
         });
     }
 }
